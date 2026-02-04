00a719719d1fa6e0b72bd35e8e257c739b6ce7f8
Douwe <238040988+douwebrinkman@users.noreply.github.com>
2026-02-04 17:10:35 +0100

    message

---
+++ src/main/java/org/Douwe/limited_life_v2/Limited_life_v2.java
@@ -0,0 +1,10 @@
+package org.Douwe.limited_life_v2;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Limited_life_v2 implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
